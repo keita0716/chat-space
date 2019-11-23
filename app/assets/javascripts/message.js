16bd10ec13c5630b361a12e49984ad427a3a694e
鈴木啓太 <suzukikeita@suzukikeitanoMacBook-Pro.local>
2019-11-23 12:09:28 +0900

    自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,118 @@
 $(function() {
 
+  var buildMessageHTML = function(message) {
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image.url + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="upper-message">` +
+          `<div class="upper-message__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="upper-message__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src="` + message.image.url + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
+
+
+  var reloadMessages = function() {
+    // 自動更新が必要ない画面では行わないようにする
+    // ⇨/groups/と/messagesの文字列が存在するURLの時のみ処理を行う
+    if(location.href.indexOf('/groups/') !== -1){
+      if(location.href.indexOf('/messages') !== -1){
+      } else {
+        return
+      }
+    }else{
+      return
+    }
+    var urlchilds = location.href.split('/');
+    urlchilds.splice(-1, 0, 'api')
+    url = urlchilds.join('/')
+    
+
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    messageAll = document.getElementsByClassName('message')
+    var last_message_id = 0
+    var num = 0
+    $.each(messageAll,function(index,message){
+      num = $(message).data('message-id')
+      if(last_message_id < num){
+        last_message_id = num
+      }
+    });
+
+
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: url,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages,function(index,message){
+        //メッセージが入ったHTMLを取得
+        insertHTML = insertHTML + buildMessageHTML(message)
+      });
+      //メッセージを追加
+      $('.messages').append(insertHTML);     
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');  
+    })
+    .fail(function() {
+      console.log('error');
+    });
+
+  };
+
+
+
   function buildHTML(message){
     var imageHtml;
     message.image.url ? imageHtml = "<img src=" + message.image.url + ">" : imageHtml = ""
@@ -24,7 +137,6 @@ $(function() {
  
   }
 
-
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -47,4 +159,15 @@ $(function() {
       alert("メッセージ送信に失敗しました");
     })
   })
+
+
+
+
+ 
+
+  setInterval(reloadMessages, 7000);
 })
+
+
+
+
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,4 +2,5 @@
   json.image  @message.image
   json.group_id  @message.group_id
   json.user_name @message.user.name
-  json.date @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
+  json.date @message.created_at.strftime("%Y年%m月%d日 %H時%M分")#idもデータとして渡す
+json.id @message.id
