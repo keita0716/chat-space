abd23b1de44d86f04b7d755ca51c4b1a6f29c887
鈴木啓太 <suzukikeita@suzukikeitanoMacBook-Pro.local>
2019-11-25 21:11:29 +0900

    c

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
+    @messages = Message.all
   end
 
   def create
